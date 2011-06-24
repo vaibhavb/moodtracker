3a429c3b5c6f201f1be31e3e418b592bff39e89e
Vaibhav Bhandari <vaibhavb@gmail.com>
2011-06-24 09:58:22 -0700

    Adding HealthVault Support for third blog post

--- MoodTracker/MyMood.xaml.cs
+++ MoodTracker/MyMood.xaml.cs
@@ -9,16 +9,111 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml;
 using Microsoft.Phone.Controls;
+
+using Microsoft.Health.Mobile;
+using Microsoft.Phone.Tasks;
+using System.Windows.Navigation;
+using System.Xml.Linq;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace MoodTracker
 {
+
+    /// <summary>
+    /// This is the main page which displays to the user if 
+    /// they go through the HealthVault authentication and authorization.
+    /// </summary>
     public partial class MainPage : PhoneApplicationPage
     {
+        public const string SettingsFilename = "Settings.xml";
+        bool _addingRecord;
+        List<string> _currentThingIds = new List<string>();
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+            Loaded += new RoutedEventHandler(MainPage_Loaded);
         }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            App.HealthVaultService.LoadSettings(SettingsFilename);
+            App.HealthVaultService.BeginAuthenticationCheck(AuthenticationCompleted, 
+                DoShellAuthentication);
+            SetProgressBarVisibility(true);
+        }
+
+        void DoShellAuthentication(object sender, HealthVaultResponseEventArgs e)
+        {
+            SetProgressBarVisibility(false);
+
+            App.HealthVaultService.SaveSettings(SettingsFilename);
+
+            string url;
+
+            if (_addingRecord)
+            {
+                url = App.HealthVaultService.GetUserAuthorizationUrl();
+            }
+            else
+            {
+                url = App.HealthVaultService.GetApplicationCreationUrl();
+            }
+
+            App.HealthVaultShellUrl = url;
+
+            // If we are  using hosted browser via the hosted browser page
+            Uri pageUri = new Uri("/HostedBrowser.xaml", UriKind.RelativeOrAbsolute);
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                NavigationService.Navigate(pageUri);
+            });
+
+        }
+
+        void AuthenticationCompleted(object sender, HealthVaultResponseEventArgs e)
+        {
+            SetProgressBarVisibility(false);
+
+            if (e != null && e.ErrorText != null)
+            {
+                SetRecordName(e.ErrorText);
+                return;
+            }
+
+            if (App.HealthVaultService.CurrentRecord == null)
+            {
+                App.HealthVaultService.CurrentRecord = App.HealthVaultService.Records[0];
+            }
+
+            App.HealthVaultService.SaveSettings(SettingsFilename);
+            if (App.HealthVaultService.CurrentRecord != null)
+            {
+                SetRecordName(App.HealthVaultService.CurrentRecord.RecordName);
+            }
+        }
+
+        void SetRecordName(string recordName)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                c_RecordName.Text = recordName;
+                c_RecordImage.Source = null;
+            });
+        }
+
+        void SetProgressBarVisibility(bool visible)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                c_progressBar.Visibility = visible ? Visibility.Visible : Visibility.Collapsed;
+            });
+        }
+
     }
 }
